60cdf203d4310e5299febb6dff4189c4a30b8803
BANGWON-LEE <74960408+BANGWON-LEE@users.noreply.github.com>
2023-01-13 13:02:53 +0900

    20230113 button mix success

--- lib/screen/tab_game.dart
+++ lib/screen/tab_game.dart
@@ -7,41 +7,87 @@ class TabGame extends StatefulWidget {
 
   @override
   State<TabGame> createState() => _TabGameState();
+
 }
 
 class _TabGameState extends State<TabGame> {
-  List<int> btnCount = [123,456,789];
 
-  
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print('dispose 실행');
+    super.dispose();
+  }
+
+  // List<int> btnCount = [123,456,789];
+  var btnCount = [['1','2','3'],['4','5','6'],['7','8','9']];
+  List gameCount = ['1','2','3','4','5','6','7','8','9'];
+  List choiceCount = [];
+  // var btnCount = [[1,2,3],[4,5,6],[7,8,9]];
+  bool startSignal = false;
+  int cnt  = -1;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Padding(
+              padding: const EdgeInsets.only(top:100.0),
+              child: Column(
+                children: [
+                  Container(
+                    child: startSignal == false ? null : Text('${gameCount}', style:TextStyle(fontSize: 40, color: Colors.black)),
+                  ),
+                  Container(
+                    child: startSignal == false ? null : Text('${choiceCount}', style:TextStyle(fontSize: 40, color: Colors.black)),
+                  )
+                ],
+              ),
+            ),
             Expanded(
               child: Container(
                 width: MediaQuery.of(context).size.width,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-
                   children: btnCount.map((el) => Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: el.toString().split('').asMap().entries.map((num) =>
+                    children: el.map((num) =>
                         // el.toString().split('').shuffle();
                       Padding(
-                        padding: EdgeInsets.only(left: num.key  == 0 ? 0 : 10.0, top: 10.0),
+                        padding: EdgeInsets.only(left:  10.0, top: 10.0),
                         child: SizedBox(
                           width: 100.0,
                           height: 100.0,
                           child: ElevatedButton(
                             onPressed:(){
 
+
+                              setState(() {
+                                cnt = cnt+1;
+                                choiceCount.add(num);
+                              });
+
+                              if(gameCount[cnt] != choiceCount[cnt]){
+                                choiceCount.remove(choiceCount[cnt]);
+                                cnt = cnt - 1;
+                              }
+
+                              if(cnt == 8 && gameCount[8] == choiceCount[8]){
+                                startSignal = false;
+                                choiceCount.clear();
+                                // dispose();
+                              }
+                              print('cnt ${cnt}');
+
+
+
                             },
                             child:
                               Text(
-                              '${num.value}',
+                              '${num}',
                                 style: TextStyle(fontSize: 40, color: Colors.white),
                             )
                           ),
@@ -55,22 +101,26 @@ class _TabGameState extends State<TabGame> {
             SizedBox(
               width: MediaQuery.of(context).size.width,
               height: 70.0,
-              child: ElevatedButton(
+              child: startSignal == true ? null : ElevatedButton(
                 onPressed: (){
-                  List<int> ranArray = [];
-                  for(int i = 0; i<3; i++){
-                   List<String> line = btnCount[i].toString().split('');
-                   line.shuffle();
-                   int num = int.parse(line.join());
-                   // int num = int.parse();
-                    // print(line);
-                    ranArray.add(num);
-                  }
-                  btnCount = ranArray;
+                  List<List<String>> resultArray = [[],[],[]];
+                  List mix = btnCount.reduce((acr,cur) => [...acr, ...cur]);
+                  mix.shuffle();
+
+                  int j = 0;
+                  int z = 0;
+
+                 for(int i = 0; i<=2; i++){
+                    for(j = j; j<=(2+z); j++){
+                      resultArray[i].add(mix[j]);
+                    }
+                    z = z + 3;
+                 }
+                  btnCount = resultArray;
                  setState(() {
-
                     btnCount.shuffle();
-
+                    gameCount.shuffle();
+                    startSignal = true;
                   });
                 } ,
                 child: Text(
